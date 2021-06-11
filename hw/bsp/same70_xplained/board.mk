7f29b3689c4388a49f4c14c87c775b3417257cb2
Rafael Silva <perigoso@riseup.net>
2021-06-11 22:50:00 +0100

    dcd: sam7x: rename family dcd to include the whole family
    Signed-off-by: Rafael Silva <perigoso@riseup.net>

--- hw/bsp/same70_xplained/board.mk
+++ hw/bsp/same70_xplained/board.mk
@@ -19,7 +19,7 @@ ASF_DIR = hw/mcu/microchip/same70
 LD_FILE = $(ASF_DIR)/same70b/gcc/gcc/same70q21b_flash.ld
 
 SRC_C += \
-	src/portable/microchip/same70/dcd_same70.c \
+	src/portable/microchip/sam7x/dcd_sam7x.c \
 	$(ASF_DIR)/same70b/gcc/gcc/startup_same70q21b.c \
 	$(ASF_DIR)/same70b/gcc/system_same70q21b.c \
 	$(ASF_DIR)/hpl/core/hpl_init.c \
